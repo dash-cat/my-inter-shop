22132a08a2c0785221a03e3708a7fc4cf0e7ef96
sudgr <122168129+sudgr@users.noreply.github.com>
2023-05-29 18:51:55 +0700

    first commit

---
+++ server/models/models.js
@@ -0,0 +1,30 @@
+const sequelize = require('../db')
+const { DataTypes } = require('sequelize')
+
+const User = sequelize.define( 'user', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'}
+})
+
+const Basket = sequelize.define( 'basket', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketDevice = sequelize.define( 'basket_device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Device = sequelize.define( 'device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false },
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    raiting: {type: DataTypes.INTEGER, unique: true, defaultValue: 0 },
+    img: {type: DataTypes.STRING, allowNull: false, allowNull: false },
+})
+
+const Type = sequelize.define('type', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true}
+})
